e0c7fd3d4c1c3bd1b645f0fd163990f28ac4b83d
komron-dev <fayziboev.dev@gmail.com>
2024-10-10 23:48:06 +0200

    add tokens

---
+++ token/paseto_maker.go
@@ -0,0 +1,46 @@
+package token
+
+import (
+	"fmt"
+	"github.com/o1egl/paseto"
+	"golang.org/x/crypto/chacha20poly1305"
+	"time"
+)
+
+type PasetoMaker struct {
+	paseto    *paseto.V2
+	secretKey string
+}
+
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	return maker.paseto.Encrypt([]byte(maker.secretKey), payload, nil)
+}
+
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+
+	err := maker.paseto.Decrypt(token, []byte(maker.secretKey), payload, nil)
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, ErrExpiredToken
+	}
+
+	return payload, nil
+}
+
+func NewPasetoMaker(secretKey string) (Maker, error) {
+	if len(secretKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size: must be exactly %d characters", chacha20poly1305.KeySize)
+	}
+
+	return &PasetoMaker{paseto.NewV2(), secretKey}, nil
+}
