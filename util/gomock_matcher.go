3478e4375dbd5c8fd542fe531e12540b100fbc1a
komron-dev <fayziboev.dev@gmail.com>
2024-10-10 17:09:05 +0200

    create user

---
+++ util/gomock_matcher.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"fmt"
+	"github.com/golang/mock/gomock"
+	db "github.com/komron-dev/bank/db/sqlc"
+	"reflect"
+)
+
+type eqCreateUserParamsMatcher struct {
+	arg      db.CreateUserParams
+	password string
+}
+
+func (e eqCreateUserParamsMatcher) Matches(x interface{}) bool {
+	arg, ok := x.(db.CreateUserParams)
+	if !ok {
+		return false
+	}
+
+	err := CheckPassword(e.password, arg.HashedPassword)
+	if err != nil {
+		return false
+	}
+
+	e.arg.HashedPassword = arg.HashedPassword
+
+	return reflect.DeepEqual(e.arg, arg)
+}
+
+func (e eqCreateUserParamsMatcher) String() string {
+	return fmt.Sprintf("matches arg %v and password %v", e.arg, e.password)
+}
+
+func EqCreateUserParams(arg db.CreateUserParams, password string) gomock.Matcher {
+	return eqCreateUserParamsMatcher{arg, password}
+}
