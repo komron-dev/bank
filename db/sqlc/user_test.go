88f6669a16657b55535bccf1d525b436130b1848
komron-dev <fayziboev.dev@gmail.com>
2025-01-12 13:17:42 +0100

    update user with optional parameters

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"github.com/komron-dev/bank/util"
 	"github.com/stretchr/testify/require"
 	"testing"
@@ -50,4 +51,129 @@ func TestGetUser(t *testing.T) {
 
 	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
 	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+}
+
+func TestUpdateUserOnlyFullName(t *testing.T) {
+	oldUser := createRandomUser(t)
+
+	newFullName := util.RandomOwner()
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username: oldUser.Username,
+		HashedPassword: sql.NullString{
+			String: oldUser.HashedPassword,
+			Valid:  true,
+		},
+		Email: sql.NullString{
+			String: oldUser.Email,
+			Valid:  true,
+		},
+		FullName: sql.NullString{
+			String: newFullName,
+			Valid:  true,
+		},
+	})
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedUser)
+
+	require.Equal(t, updatedUser.HashedPassword, oldUser.HashedPassword)
+	require.Equal(t, updatedUser.Email, oldUser.Email)
+	require.Equal(t, newFullName, updatedUser.FullName)
+
+	require.NotEqual(t, updatedUser.FullName, oldUser.FullName)
+}
+
+func TestUpdateUserOnlyEmail(t *testing.T) {
+	oldUser := createRandomUser(t)
+
+	newEmail := util.RandomEmail()
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username: oldUser.Username,
+		HashedPassword: sql.NullString{
+			String: oldUser.HashedPassword,
+			Valid:  true,
+		},
+		Email: sql.NullString{
+			String: newEmail,
+			Valid:  true,
+		},
+		FullName: sql.NullString{
+			String: oldUser.FullName,
+			Valid:  true,
+		},
+	})
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedUser)
+
+	require.Equal(t, updatedUser.HashedPassword, oldUser.HashedPassword)
+	require.Equal(t, updatedUser.FullName, oldUser.FullName)
+	require.Equal(t, newEmail, updatedUser.Email)
+
+	require.NotEqual(t, updatedUser.Email, oldUser.Email)
+}
+
+func TestUpdateUserOnlyPassword(t *testing.T) {
+	oldUser := createRandomUser(t)
+
+	newPassword := util.RandomString(7)
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username: oldUser.Username,
+		HashedPassword: sql.NullString{
+			String: newPassword,
+			Valid:  true,
+		},
+		Email: sql.NullString{
+			String: oldUser.Email,
+			Valid:  true,
+		},
+		FullName: sql.NullString{
+			String: oldUser.FullName,
+			Valid:  true,
+		},
+	})
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedUser)
+
+	require.Equal(t, updatedUser.Email, oldUser.Email)
+	require.Equal(t, updatedUser.FullName, oldUser.FullName)
+	require.Equal(t, newPassword, updatedUser.HashedPassword)
+
+	require.NotEqual(t, updatedUser.HashedPassword, oldUser.HashedPassword)
+}
+
+func TestUpdateUserFull(t *testing.T) {
+	oldUser := createRandomUser(t)
+
+	newPassword := util.RandomString(7)
+	newFullName := util.RandomOwner()
+	newEmail := util.RandomEmail()
+
+	updatedUser, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username: oldUser.Username,
+		HashedPassword: sql.NullString{
+			String: newPassword,
+			Valid:  true,
+		},
+		Email: sql.NullString{
+			String: newEmail,
+			Valid:  true,
+		},
+		FullName: sql.NullString{
+			String: newFullName,
+			Valid:  true,
+		},
+	})
+
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedUser)
+
+	require.Equal(t, newFullName, updatedUser.FullName)
+	require.Equal(t, newEmail, updatedUser.Email)
+	require.Equal(t, newPassword, updatedUser.HashedPassword)
+
+	require.NotEqual(t, updatedUser.FullName, oldUser.FullName)
+	require.NotEqual(t, updatedUser.Email, oldUser.Email)
+	require.NotEqual(t, updatedUser.HashedPassword, oldUser.HashedPassword)
 }
---
+++ grpc_api/rpc_update_user.go
@@ -0,0 +1,90 @@
+package grpc_api
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	db "github.com/komron-dev/bank/db/sqlc"
+	"github.com/komron-dev/bank/pb"
+	"github.com/komron-dev/bank/util"
+	"github.com/komron-dev/bank/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"time"
+)
+
+func (server *Server) UpdateUser(ctx context.Context, request *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+	violations := validateUpdateUserRequest(request)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
+	arg := db.UpdateUserParams{
+		Username: request.GetUsername(),
+		FullName: sql.NullString{
+			String: request.GetFullName(),
+			Valid:  request.FullName != nil,
+		},
+		Email: sql.NullString{
+			String: request.GetEmail(),
+			Valid:  request.Email != nil,
+		},
+	}
+
+	if request.Password != nil {
+		hashedPassword, err := util.HashPassword(request.GetPassword())
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err)
+		}
+
+		arg.HashedPassword = sql.NullString{
+			String: hashedPassword,
+			Valid:  true,
+		}
+
+		arg.PasswordChangedAt = sql.NullTime{
+			Time:  time.Now(),
+			Valid: true,
+		}
+	}
+
+	user, err := server.store.UpdateUser(ctx, arg)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, status.Errorf(codes.NotFound, "user not found")
+		}
+		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
+	}
+
+	response := &pb.UpdateUserResponse{
+		User: convertUser(user),
+	}
+	return response, nil
+}
+
+func validateUpdateUserRequest(req *pb.UpdateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if req.FullName != nil {
+		if err := val.ValidateFullName(req.GetFullName()); err != nil {
+			violations = append(violations, fieldViolation("full_name", err))
+		}
+	}
+
+	if req.Password != nil {
+		if err := val.ValidatePassword(req.GetPassword()); err != nil {
+			violations = append(violations, fieldViolation("password", err))
+		}
+	}
+
+	if req.Email != nil {
+		if err := val.ValidateEmail(req.GetEmail()); err != nil {
+			violations = append(violations, fieldViolation("email", err))
+		}
+	}
+
+	return violations
+}
